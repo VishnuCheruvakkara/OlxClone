fd76ff1995256751695f654e0620da931ee231e0
vishnu cheruvakkara <cheruvakkarav@gmail.com>
2025-01-06 00:07:52 +0530

    .env setup completed!

--- src/firebase/FireBase.jsx
+++ src/firebase/FireBase.jsx
@@ -4,13 +4,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth"
 import {getFirestore} from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCoPdzYGTSIGiNt0eKVJG0UN-o7wUKo564",
-  authDomain: "olxclone-eacfb.firebaseapp.com",
-  projectId: "olxclone-eacfb",
-  storageBucket: "olxclone-eacfb.firebasestorage.app",
-  messagingSenderId: "216821994934",
-  appId: "1:216821994934:web:f8b3ef03d5b29054da5d17",
-  measurementId: "G-KGS2BERX4J"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 
